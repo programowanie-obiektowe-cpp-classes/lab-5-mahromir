0775fdf785d31c9d787a286d010c3651161e5964
Marek Polewski <marek.polewski.stud@pw.edu.pl>
2025-11-21 18:26:06 +0100

    Lab5 Marek Polewski

--- foo.hpp
+++ foo.hpp
@@ -7,6 +7,16 @@
 
 std::vector< char > foo(std::list< Human >& people)
 {
-    // Twoja implementacja tutaj
-    return {};
+    std::vector<char> characters;
+    for(auto it=people.begin(); it!=people.end(); it++){
+        // Nie lubi pieskow ani kotkow
+        bool monster = it->isMonster();
+        if (!monster){
+            characters.insert(characters.begin(), 'y');  
+        }else{
+            characters.insert(characters.begin(), 'n');  
+        }             
+        it->birthday();
+    }
+    return characters;
 }
